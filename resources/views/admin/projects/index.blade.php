45e80d955dd208def0acc09a9ec006b9f0ba9248
giovannimegliola <giovanni.megliola@hotmail.it>
2024-01-12 15:35:16 +0100

    organized projects list inside a table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,16 +3,29 @@
     <section class="container">
         <h1>Projects List</h1>
 
-        @foreach ($projects as $project)
-        <p>
-            {{$project->title}}
-        </p>
-        <div class="my-3">
-            <a href="{{route('admin.projects.show', $project->id)}}" class="btn btn-primary"><i class="fa-solid fa-eye"></i></a>
-            <a href="{{route('admin.projects.edit', $project->id)}}" class="btn btn-secondary"><i class="fa-solid fa-pen"></i></a>
-            <a href="{{route('admin.projects.destroy', $project->id)}}" class="btn btn-danger"><i class="fa-solid fa-trash"></i></a>
-        </div>
+        <a href="{{route('admin.projects.create')}}" class="btn btn-primary my-3">Create new Project</a>
 
-        @endforeach
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project)
+                    <tr>
+                        <td>{{$project->title}}</td>
+                        <td>
+                            <a href="{{route('admin.projects.show', $project->id)}}" class="btn btn-primary"><i class="fa-solid fa-eye"></i></a>
+                            <a href="{{route('admin.projects.edit', $project->id)}}" class="btn btn-secondary"><i class="fa-solid fa-pen"></i></a>
+                            <a href="{{route('admin.projects.destroy', $project->id)}}" class="btn btn-danger"><i class="fa-solid fa-trash"></i></a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+
     </section>
 @endsection
