79421a29124d56471f513099c870f594ebf5be5c
giovannimegliola <giovanni.megliola@hotmail.it>
2024-01-14 23:12:49 +0100

    modified data in db

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -5,32 +5,35 @@
 
         <a href="{{route('admin.projects.create')}}" class="btn btn-primary my-3"><i class="fa-solid fa-plus"></i> Create new Project</a>
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($projects as $project)
+        <div class="container">
+            <table class="table">
+                <thead>
                     <tr>
-                        <td>{{$project->title}}</td>
-                        <td class="d-flex">
-                            <a href="{{route('admin.projects.show', $project->id)}}" class="btn btn-primary me-2"><i class="fa-solid fa-eye"></i></a>
-
-                            <a href="{{route('admin.projects.edit', $project->id)}}" class="btn btn-secondary me-2"><i class="fa-solid fa-pen"></i></a>
-
-                            <form action="{{route('admin.projects.destroy', $project->id)}}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="cancel-button btn btn-danger" data-item-title="{{$project->title}}"><i class="fa-solid fa-trash"></i></button>
-                            </form>
-                        </td>
+                        <th>Title</th>
+                        <th>Actions</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($projects as $project)
+                        <tr>
+                            <td>{{$project->title}}</td>
+                            <td class="d-flex">
+                                <a href="{{route('admin.projects.show', $project->id)}}" class="btn btn-primary me-2"><i class="fa-solid fa-eye"></i></a>
+
+                                <a href="{{route('admin.projects.edit', $project->id)}}" class="btn btn-secondary me-2"><i class="fa-solid fa-pen"></i></a>
+
+                                <form action="{{route('admin.projects.destroy', $project->id)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="cancel-button btn btn-danger" data-item-title="{{$project->title}}"><i class="fa-solid fa-trash"></i></button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
         @include('profile/partials.modal_delete')
     </section>
 @endsection
